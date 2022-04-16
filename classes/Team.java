4f6d2ed62cf9327537c8255c245f260ef576b650
cfreer <cfreer@uw.edu>
2022-04-15 18:39:30 -0700

    add addPlayer

--- classes/Team.java
+++ classes/Team.java
@@ -1,14 +1,35 @@
 package classes;
+import java.util.*;
 
 // A Team represents a list of 12 Players that is able to be modified and
 // also keeps track of a salary cap.
 public class Team {
-  private Player[] players;
+  private List<Player> players;
+  private double totalSalary;
   private double salaryCap;
 
-  public Team(Player[] players, double salaryCap) {
-    this.players = players;
+  public Team(double salaryCap) {
+    this.players = new ArrayList<Player>();
     this.salaryCap = salaryCap;
+    this.totalSalary = 0;
+  }
+
+  public boolean addPlayer(Player player) {
+    if (this.players.size() < 12) {
+      if (this.totalSalary + player.salary > this.salaryCap) {
+        // TODO: display this message to user in UI
+        System.out.println("Error: Player cannot be added to the team." +
+                            "Player's salary causes the total team salary to exceed the salary cap.");
+      } else {
+        this.players.add(player);
+        this.totalSalary += player.salary;
+        return true;
+      }
+    } else {
+      System.out.println("Error: Player cannot be added to the team." +
+                            "Team already has 12 players.");
+    }
+    return false;
   }
 
 }
