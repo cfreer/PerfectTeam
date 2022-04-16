d3a50aa99e4e054ea8ecbf498d91a45bb1cc8444
nedwardh <42857990+nedwardh@users.noreply.github.com>
2022-04-15 17:28:13 -0700

    Update Algorithm.java

--- classes/Algorithm.java
+++ classes/Algorithm.java
@@ -1,5 +1,8 @@
 package classes;
 
+//This is the Alrgorithm class for PerfectTeam.
+//This will take any potential 12 person team and predict its potential Win/Loss record over a season.
+
 public class Algorithm {
 
 }
