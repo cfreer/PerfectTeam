aa9e979c91131344c64ea2550ff0dc04cb3bd719
cfreer <cfreer@uw.edu>
2022-04-15 17:20:37 -0700

    java outline files

---
+++ Player.java
@@ -0,0 +1,35 @@
+public class Player {
+  private Stats stats;
+  private PER per;
+  private WinShares winShares;
+  private double salary;
+  private double projSalary;
+
+  public Player(Stats stats, PER per, WinShares winShares, double salary, double projSalary) {
+    this.stats = stats;
+    this.per = per;
+    this.winShares = winShares;
+    this.salary = salary;
+    this.projSalary = projSalary;
+  }
+
+  private class Stats {
+    public Stats() {
+
+    }
+  }
+
+  private class PER {
+    public PER() {
+
+    }
+  }
+
+  private class WinShares {
+    public WinShares() {
+
+    }
+
+  }
+
+}
---
+++ Team.java
@@ -0,0 +1,8 @@
+public class Team {
+  private Player[] players;
+
+  public Team(Player[] players) {
+    this.players = players;
+  }
+
+}
