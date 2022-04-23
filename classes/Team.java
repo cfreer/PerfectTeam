2cc439807cf5a29cecd6abdd9bdf02b01b715afc
cfreer <cfreer@uw.edu>
2022-04-22 18:25:14 -0700

    add player and team basic functionality and doc

--- classes/Team.java
+++ classes/Team.java
@@ -4,16 +4,26 @@ import java.util.*;
 // A Team represents a list of 12 Players that is able to be modified and
 // also keeps track of a salary cap.
 public class Team {
-  private List<Player> players;
-  private double totalSalary;
-  private double salaryCap;
+  private List<Player> players; // 12 players on the team.
+  private double totalSalary; // Total salary of the team.
+  private double salaryCap; // Maximum salary allowed of the team.
 
+  /**
+   * Creates a new Team with the given salary cap.
+   * @param salaryCap Maximum salary allowed of the team.
+   */
   public Team(double salaryCap) {
     this.players = new ArrayList<Player>();
     this.salaryCap = salaryCap;
     this.totalSalary = 0;
   }
 
+  /**
+   * If possible, adds the given player to the current team. If not, displays an
+   * error to the user.
+   * @param player The player to be added to the team.
+   * @return True if the player is successfully added to the team and false if not.
+   */
   public boolean addPlayer(Player player) {
     double salary = player.getSalary();
     if (this.players.size() < 12) {
@@ -33,8 +43,20 @@ public class Team {
     return false;
   }
 
+  /**
+   * If on the team, removes the given player to the current team.
+   * @param player The player to be removed from the team.
+   * @return True if the player is successfully removed from the team and false if not.
+   */
   public boolean removePlayer(Player player) {
     return this.players.remove(player);
   }
 
+  /**
+   * Sets the salary cap of the team to the given salary cap.
+   * @param salaryCap Maximum salary allowed of the team.
+   */
+  public void setSalaryCap(double salaryCap) {
+    this.salaryCap = salaryCap;
+  }
 }
