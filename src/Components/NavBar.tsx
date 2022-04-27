2c40802e0f6a9b9febc96e17d46ae71f1bbb0492
aaron <aaronjc9@uw.edu>
2022-04-27 13:34:57 -0700

    Frontend Structure

---
+++ src/Components/NavBar.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+const navbar = () => {
+    return (
+        <div>
+            <li>
+                <Link to="/">Create a Team</Link>
+            </li>
+            <li>
+                <Link to="/">Games</Link>
+            </li>
+            <li>
+                <Link to="/">Teams</Link>
+            </li>
+            <li>
+                <Link to="/">Players</Link>
+            </li>
+        </div>
+    )
+}
