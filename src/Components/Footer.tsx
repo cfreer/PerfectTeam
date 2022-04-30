42391a7ca78876ff8abe150074a3992d024e54f9
Amanda Ki <aki18@uw.edu>
2022-04-30 06:41:30 -0700

    Add footer component

---
+++ src/Components/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <div id="footer">
+        <p>Portions of this page &copy;2022 Aaron Carter, Amanda Ki, Caroline Freer, Juan Gonzalez, Ned Harrison</p>
+    </div>
+  )
+}
+
+export default Footer;
