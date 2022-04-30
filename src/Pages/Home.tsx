71a5ad9f41e3854c47d7fddcf87a2d1dd3e6575b
Amanda Ki <aki18@uw.edu>
2022-04-30 10:42:47 -0700

    Add routes

---
+++ src/Pages/Home.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import Home from './pages/Home.ts';
+import CreateTeam from './pages/CreateTeam';
+import Games from './pages/Games';
+import Teams from './pages/Teams';
+import Players from './pages/Players';
+import './index.css';
+import Footer from './components/Footer';
+import NavBar from './components/NavBar';
+
+
+function App(props : any) {
+
+  // returns rendered routes and specified components
+  return (
+    <div>
+      <Route path="/" component={NavBar} />
+      <main>
+        <Switch>
+          <Route path="/" >
+            <Home />
+          </Route>
+          <Route path="/createteam">
+            <CreateTeam />
+          </Route>
+          <Route path="/games">
+            <Games />
+          </Route>
+          <Route path="/teams">
+            <Teams />
+          </Route>
+          <Route path="/players" >
+            <Players />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </main>
+      <footer>
+        <Route path="/" component={Footer} />
+      </footer>
+    </div>
+  );
+}
+
+export default App;
