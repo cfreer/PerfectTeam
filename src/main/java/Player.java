3efaf21fe654b8f27ac8c66d5dcdc697d6702624
Amanda Ki <aki18@uw.edu>
2022-05-01 22:10:21 -0700

    Add java files back to src/main/java

---
+++ src/main/java/Player.java
@@ -0,0 +1,239 @@
+package src.main.java;
+import java.awt.*;
+
+// A Player represents a basketball player that has stats, a PER, win shares,
+// salary, a projected salary, profile picture, name, and age.
+public class Player {
+  private Stats stats; //points, assists, defensive rebounds, offensive rebounds, steals, turnovers, personal fouls,
+                       //blocks, blocks against, free throws made
+  private double per; // Player Efficiency Rating: A measure of per-minute
+                      // production standardized such that the league average is 15.
+  private double winShares; // An estimate of the number of wins contributed by a player.
+  private double salary; // Player's salary.
+  private double projSalary; // Player's projected salary.
+  private Image proPic; // Player's profile picture.
+  private String name; // Player's name in the form "[FirstName] [LastName]".
+  private int age; // Player's age.
+
+  /**
+   * Creates a new Player with the given data.
+   * @param stats Averages of a player's points, free throws made, offensive rebounds, defensive
+   *              rebounds, assists, turnovers, steals, blocks, blocks against, and personal fouls.
+   * @param per Player Efficiency Rating.
+   * @param winShares An estimate of the number of wins contributed by a player.
+   * @param salary Player's salary.
+   * @param projSalary Player's projected salary.
+   * @param proPic Player's profile picture.
+   * @param name Player's name in the form "[FirstName] [LastName]".
+   * @param age Player's age.
+   */
+  public Player(Stats stats, double per, double winShares, double salary, double projSalary,
+                Image proPic, String name, int age) {
+    this.stats = stats;
+    this.per = per;
+    this.winShares = winShares;
+    this.salary = salary;
+    this.projSalary = projSalary;
+    this.proPic = proPic;
+    this.name = name;
+    this.age = age;
+  }
+
+  /**
+   * Returns the player's stats.
+   * @return Player's stats.
+   */
+  public Stats getStats() {
+    return this.stats;
+  }
+
+  /**
+   * Returns the player's player efficiency rating.
+   * @return Player's player efficiency rating.
+   */
+  public double getPER() {
+    return this.per;
+  }
+
+  /**
+   * Returns the player's win shares.
+   * @return Player's win shares.
+   */
+  public double getWinShares() {
+    return this.winShares;
+  }
+
+  /**
+   * Returns the player's salary.
+   * @return Player's salary.
+   */
+  public double getSalary() {
+    return this.salary;
+  }
+
+  /**
+   * Returns the player's projected salary.
+   * @return Player's projected salary.
+   */
+  public double getProjSalary() {
+    return this.projSalary;
+  }
+
+  /**
+   * Returns the player's profile picture.
+   * @return Player's profile picture.
+   */
+  public Image getProPic() {
+    return this.proPic;
+  }
+
+  /**
+   * Returns the player's first and last name.
+   * @return Player's first and last name.
+   */
+  public String getName() {
+    return this.name;
+  }
+
+  /**
+   * Returns the player's age.
+   * @return Player's age.
+   */
+  public int getAge() {
+    return this.age;
+  }
+
+  // A Stats represents averages of a player's points, free throws made, offensive rebounds,
+  // defensive rebounds, assists, turnovers, steals, blocks, blocks against, and personal fouls.
+  public class Stats {
+    private double points; // Average points.
+    private double freeThrows; // Average free throws made.
+    private double offRebounds; // Average offensive rebounds.
+    private double defRebounds; // Average defensive rebounds.
+    private double assists; // Average assists.
+    private double turnovers; // Average turnovers.
+    private double steals; // Average steals.
+    private double blocks; // Average blocks.
+    private double blocksAgainst; // Average blocks against.
+    private double personalFouls; // Average personal fouls.
+    private double playingTime; //Average playing time.
+
+    /**
+     * Creates a new Stats with the given data.
+     * @param points Average points.
+     * @param freeThrows Average free throws made.
+     * @param offRebounds Average offensive rebounds.
+     * @param defRebounds Average defensive rebounds.
+     * @param assists Average assists.
+     * @param turnovers Average turnovers.
+     * @param steals Average steals.
+     * @param blocks Average blocks.
+     * @param blocksAgainst Average blocks against.
+     * @param personalFouls Average personal fouls.
+     */
+    public Stats(double points, double freeThrows, double offRebounds, double defRebounds, double assists,
+                 double turnovers, double steals, double blocks, double blocksAgainst, double personalFouls, double playingTime) {
+      this.points = points;
+      this.freeThrows = freeThrows;
+      this.offRebounds = offRebounds;
+      this.defRebounds = defRebounds;
+      this.assists = assists;
+      this.turnovers = turnovers;
+      this.steals = steals;
+      this.blocks = blocks;
+      this.blocksAgainst = blocksAgainst;
+      this.personalFouls = personalFouls;
+      this.playingTime = playingTime;
+    }
+
+    /**
+     * Returns the average points.
+     * @return Average points.
+     */
+    public double getPoints() {
+      return this.points;
+    }
+
+    /**
+     * Returns the playing time.
+     * @return Playing time of the player.
+     */
+    public double getPlayingTime() {
+      return this.playingTime;
+    }
+
+    /**
+     * Returns the average free throws made.
+     * @return Average free throws made.
+     */
+    public double getFreeThrows() {
+      return this.freeThrows;
+    }
+
+    /**
+     * Returns the average offensive rebounds.
+     * @return Average offensive rebounds.
+     */
+    public double getOffRebounds() {
+      return this.offRebounds;
+    }
+
+    /**
+     * Returns the average defensive rebounds.
+     * @return Average defensive rebounds.
+     */
+    public double getDefRebounds() {
+      return this.defRebounds;
+    }
+
+    /**
+     * Returns the average assists.
+     * @return Average assists.
+     */
+    public double getAssists() {
+      return this.assists;
+    }
+
+    /**
+     * Returns the average turnovers.
+     * @return Average turnovers.
+     */
+    public double getTurnovers() {
+      return this.turnovers;
+    }
+
+    /**
+     * Returns the average steals.
+     * @return Average steals.
+     */
+    public double getSteals() {
+      return this.steals;
+    }
+
+    /**
+     * Returns the average blocks.
+     * @return Average blocks.
+     */
+    public double getBlocks() {
+      return this.blocks;
+    }
+
+    /**
+     * Returns the average blocks against.
+     * @return Average blocks against.
+     */
+    public double getBlocksAgainst() {
+      return this.blocksAgainst;
+    }
+
+    /**
+     * Returns the average personal fouls.
+     * @return Average personal fouls.
+     */
+    public double getPersonalFouls() {
+      return this.personalFouls;
+    }
+
+  }
+
+}
---
+++ src/main/java/Team.java
@@ -0,0 +1,81 @@
+package src.main.java;
+import java.util.*;
+
+// A Team represents a list of 12 Players that is able to be modified and
+// also keeps track of a salary cap.
+public class Team {
+  private List<Player> players; // 12 players on the team.
+  private double totalSalary; // Total salary of the team.
+  private double salaryCap; // Maximum salary allowed of the team.
+
+  /**
+   * Creates a new Team with the given salary cap.
+   * @param salaryCap Maximum salary allowed of the team.
+   */
+  public Team(double salaryCap) {
+    this.players = new ArrayList<Player>();
+    this.salaryCap = salaryCap;
+    this.totalSalary = 0;
+  }
+
+  //get totalSalary
+
+
+  /**
+   * Returns the teams's total salary.
+   * @return Team's total salary.
+   */
+  public double getTotalSalary() {
+    return this.totalSalary;
+  }
+
+  /**
+   * @return The list of players on the team
+   */
+
+  public List<Player> getTeam() {
+    return players;
+  }
+
+  /**
+   * If possible, adds the given player to the current team. If not, displays an
+   * error to the user.
+   * @param player The player to be added to the team.
+   * @return True if the player is successfully added to the team and false if not.
+   */
+  public boolean addPlayer(Player player) {
+    double salary = player.getSalary();
+    if (this.players.size() < 12) {
+      if (this.totalSalary + salary > this.salaryCap) {
+        // TODO: display this message to user in UI
+        System.out.println("Error: Player cannot be added to the team." +
+                            "Player's salary causes the total team salary to exceed the salary cap.");
+      } else {
+        this.players.add(player);
+        this.totalSalary += salary;
+        return true;
+      }
+    } else {
+      System.out.println("Error: Player cannot be added to the team." +
+                            "Team already has 12 players.");
+    }
+    return false;
+  }
+
+  /**
+   * If on the team, removes the given player to the current team.
+   * @param player The player to be removed from the team.
+   * @return True if the player is successfully removed from the team and false if not.
+   */
+  public boolean removePlayer(Player player) {
+    return this.players.remove(player);
+  }
+
+  /**
+   * Sets the salary cap of the team to the given salary cap.
+   * @param salaryCap Maximum salary allowed of the team.
+   */
+  public void setSalaryCap(double salaryCap) {
+    this.salaryCap = salaryCap;
+  }
+}
