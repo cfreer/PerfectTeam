2417f4fe417fd9a7ba791a437374f6cadb243980
Amanda Ki <aki18@uw.edu>
2022-05-03 13:00:59 -0700

    Add slideshow to home page

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import Slideshow from '../components/Slideshow';
 
 function Home(props : any) {
 
   // renders home page
   return (
     <div className='home-container'>
-      <p>home page</p>
+      <Slideshow />
+      <div id='tutorial-description'>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ornare arcu dui
+          vivamus arcu felis bibendum ut. Gravida quis blandit turpis cursus in
+          hac habitasse platea dictumst. Sagittis nisl rhoncus mattis rhoncus.
+        </p>
+        <Button variant='primary'>Create Team</Button>
+      </div>
+
     </div>
   );
 }
