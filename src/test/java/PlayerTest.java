556c24587371933fffcba32fb75b27cf6ff3358b
Amanda Ki <aki18@uw.edu>
2022-05-03 03:38:40 -0700

    Add PlayerTest.java back to src

---
+++ src/test/java/PlayerTest.java
@@ -0,0 +1,13 @@
+package src.test.java;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+    private static double EPS = 1e-9;
+
+    @Test
+    public void testPractice() {
+        assertEquals (3, 3, EPS);
+    }
+}
