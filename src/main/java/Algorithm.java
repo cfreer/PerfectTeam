d8db1de2819599007f6d3594b7d77dcc67bd3a9f
nedwardh <42857990+nedwardh@users.noreply.github.com>
2022-05-06 17:05:23 -0700

    Update Algorithm.java
    Algo update

--- src/main/java/Algorithm.java
+++ src/main/java/Algorithm.java
@@ -1,10 +1,22 @@
 package src.main.java;
 
+import src.main.java.Player;
+
 //This is the Alrgorithm class for PerfectTeam.
 //This will take any potential 12 person team and predict its potential Win/Loss record over a season.
 
 public class Algorithm {
   private final double totalMins = 240.0;
+  private double pts;
+  private double ftm;
+  private double oreb;
+  private double dreb;
+  private double ast;
+  private double tov;
+  private double stl;
+  private double blk;
+  private double blka;
+  private double pf;
   /*  This takes the data from the team and stores it to be used later for calculating winP.
 
       @Spec.requires team size to be 12
@@ -14,7 +26,20 @@ public class Algorithm {
     double playingTimeScale = 0;
     double totalPlayingTime = 0.0;
     for (Player player : team.getPlayers()) {
-      player.getStats().getPlayingTime();
+      totalPlayingTime += player.getStats().getPlayingTime();
+    }
+    playingTimeScale = totalPlayingTime/totalMins;
+    for (Player player : team.getPlayers()) {
+      pts += player.getStats().getPoints() * playingTimeScale;
+      pts += player.getStats().getFreeThrows() * playingTimeScale;
+      pts += player.getStats().getOffRebounds() * playingTimeScale;
+      pts += player.getStats().getDefRebounds() * playingTimeScale;
+      pts += player.getStats().getAssists() * playingTimeScale;
+      pts += player.getStats().getTurnovers() * playingTimeScale;
+      pts += player.getStats().getSteals() * playingTimeScale;
+      pts += player.getStats().getBlocks() * playingTimeScale;
+      pts += player.getStats().getBlocksAgainst() * playingTimeScale;
+      pts += player.getStats().getPersonalFouls() * playingTimeScale;
     }
   }
   /*
