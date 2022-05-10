912caf9af0b9515d3a55eb9b81da8af1cb3a0196
aaron <aaronjc9@uw.edu>
2022-05-09 23:59:24 -0700

    search bar complete functionaility

--- client/src/Components/SearchBar.tsx
+++ client/src/Components/SearchBar.tsx
@@ -1,105 +1,63 @@
-import React, { useState } from 'react'
-import './App.css'
-import { ReactSearchAutocomplete } from 'react-search-autocomplete'
+import React, { useState } from "react";
+import { ReactSearchAutocomplete } from "react-search-autocomplete";
 
-interface Player {
-    id: string,
-    Rk: number,
-    Player: string,
-    age: number,
-    WS: string,
-    salary: string,
-    projSalary: string,
-    playerTeam: string,
-  }
+type Item = {
+  id: number;
+  name: string;
+}
 
-function SearchBar() {
+type Player = {
+  id: string,
+  Rk: number,
+  Player: string,
+  age: number,
+  WS: string,
+  salary: string,
+  projSalary: string,
+  playerTeam: string,
+}
 
+function SearchBar(props : any) {
+  const [searchString, setSearchString] = useState("");
+  const data : Player[] = props.data;
 
-  const[currentInput, setCurrentInput] = useState("");
-  const[searchString, setSearchString] = useState("");
-  const[suggestions, useSuggestions] = useState();
-  const[suggestionsInput, setsuggestionsInput] = useState("");
-  const[player, setPlayer] = useState("");
-  const [data, setData] = useState<Player[]>([]);
+  const items : Item[] = data.map((obj : Player) => ({
+    id: parseInt(obj.id),
+    name: (obj.Player).substring(0, obj.Player.indexOf('\\'))
+  }));
 
-  const API_URL = 'http://localhost:4567/';
-
-  const handleOnSearch = async (string: React.SetStateAction<string>, results: any) => {
-    // onSearch will have as the first callback parameter
-    // the string searched and for the second the results.
-    console.log(string, results)
+  const handleOnSearch = (string : string, results : unknown[]) => {
+    console.log(string, results);
     setSearchString(string);
-    setCurrentInput(string);
-    return results
-  }
+  };
 
   const handleOnClear = () => {
-      setSearchString("");
-      setCurrentInput("");
-  }
-
-  const handleOnHover = (result: any) => {
-    // the item hovered
-    console.log(result)
-  }
-
-  /*const handleOnSelect = (data: React.SetStateAction<string>) => {
-    // the item selected
-    console.log(data)
-    setPlayer(data);
+    console.log("Cleared");
     setSearchString("");
-    setCurrentInput("");
-  } */
+  };
 
-  const handleOnFocus = () => {
-    console.log('Focused')
-  }
-
-  const formatResult = (item: { id: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined; name: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined; }) => {
-    return (
-      <>
-        <span style={{ display: 'block', textAlign: 'left' }}>id: {item.id}</span>
-        <span style={{ display: 'block', textAlign: 'left' }}>name: {item.name}</span>
-      </>
-    )
-  }
-
-  function getPlayersRequest() {
-    fetch(API_URL + 'players')
-      .then(statusCheck)
-      .then(res => res.json())
-      .then(setData)
-      .catch(console.error);
-  }
-
-  async function statusCheck(res : Response) {
-    if (!res.ok) {
-      throw new Error(await res.json());
-    }
-    return res;
-  }
-  
-  getPlayersRequest();
+  const clearSearchBox = () => {
+    setSearchString("");
+  };
 
   return (
     <div className="App">
       <header className="App-header">
-        <div style={{ width: 400 }}>
+        <button onClick={clearSearchBox}>Clear input!</button>
+        <div style={{ width: 200, margin: 20 }}>
+          <div style={{ marginBottom: 20 }}>Try to type "JavaScript"</div>
           <ReactSearchAutocomplete
-            items={data}
+            items={items}
             onSearch={handleOnSearch}
-            onHover={handleOnHover}
-            //onSelect={handleOnSelect}
-            onFocus={handleOnFocus}
-            autoFocus
-            formatResult={formatResult}
             onClear={handleOnClear}
+            inputSearchString={searchString}
+            autoFocus
           />
+          <div style={{ marginTop: 20 }}>This text will be covered!</div>
         </div>
       </header>
     </div>
-  )
+  );
 }
 
 export default SearchBar;
