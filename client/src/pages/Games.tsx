d26b79ae737153b52b3c3c5f72035b10bac71c92
aaron <aaronjc9@uw.edu>
2022-05-10 17:15:51 -0700

    working on image

--- client/src/pages/Games.tsx
+++ client/src/pages/Games.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import NavBar from '../components/NavBar';
+import Footer from '../components/Footer';
+import GamesIn from './../../public/imgs/GamesIn.PNG';
 
 function Games(props : any) {
+
+  const image = require("./GamesIn.PNG").default;
 
   // rendered games page
   return (
     <div className='games-container'>
-      <p>games page</p>
+      <div>
+        <NavBar/>
+      </div>
+      <div className='game-image'>
+        <GamesIn>
+          <img src={image} alt="game_img"></img>
+        </GamesIn>
+      </div>
+      <div>
+        <Footer/>
+      </div>
     </div>
   );
 }
