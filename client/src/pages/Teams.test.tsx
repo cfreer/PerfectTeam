c60e2ad45e1bb5a08f263c3ee7b512ed3be4450d
Amanda Ki <aki18@uw.edu>
2022-05-27 10:54:29 -0700

    Update teams page to use API data

--- client/src/pages/Teams.test.tsx
+++ client/src/pages/Teams.test.tsx
@@ -2,17 +2,12 @@
  * This file contains the unit test functions for the Games
  */
 
- import { render, screen } from '@testing-library/react';
- import Teams from './Teams';
- import { BrowserRouter } from 'react-router-dom';
- 
- const currRender = (ui : any, {route = '/'} = {}) => {
-   window.history.pushState({}, 'Test page', route)
- 
-   return render(ui, {wrapper: BrowserRouter})
- }
- 
- test('renders game container', () => {
-   currRender(<Teams/>)
-   expect(screen.getByTestId(/teams-container/i)).toBeInTheDocument();
- });
+import { render, screen } from '@testing-library/react';
+import Teams from './Teams';
+import teamData from './../../data/nbaTeams.json';
+
+test('renders team table', () => {
+  render(<Teams data={teamData}/>)
+  expect(screen.getByTestId(/teams-table/i)).toBeInTheDocument();
+  expect(screen.getByTestId(/teams-table/i)).toBeVisible();
+});
