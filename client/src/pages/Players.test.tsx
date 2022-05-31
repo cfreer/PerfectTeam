4c05b2900abe4a63b2ed84edbfcb71345478ff16
Amanda Ki <aki18@uw.edu>
2022-05-31 05:38:08 -0700

    Lint

--- client/src/pages/Players.test.tsx
+++ client/src/pages/Players.test.tsx
@@ -2,22 +2,22 @@
  * This file contains the unit test functions for the Players page component.
  */
 
- import { render, screen } from '@testing-library/react';
- import Players from './Players';
- import players from './../../data/players.json';
- 
- test('renders player search bar', () => {
-   render(<Players data={players}/>)
-   expect(screen.getByTestId(/search/i)).toBeInTheDocument();
- });
- 
- test('renders search player button', () => {
-   render(<Players data={players}/>)
-   expect(screen.getByTestId(/add-btn/i)).toBeInTheDocument();
- });
- 
- 
- test('renders player cards', () => {
-   render(<Players data={players}/>)
-   expect(screen.getByTestId('player-cards')).toBeInTheDocument();
- });
+import { render, screen } from '@testing-library/react';
+import Players from './Players';
+import players from './../../data/players.json';
+
+test('renders player search bar', () => {
+  render(<Players data={players}/>)
+  expect(screen.getByTestId(/search/i)).toBeInTheDocument();
+});
+
+test('renders search player button', () => {
+  render(<Players data={players}/>)
+  expect(screen.getByTestId(/add-btn/i)).toBeInTheDocument();
+});
+
+
+test('renders player cards', () => {
+  render(<Players data={players}/>)
+  expect(screen.getByTestId('player-cards')).toBeInTheDocument();
+});
