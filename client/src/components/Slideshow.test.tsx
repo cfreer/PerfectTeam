e21a17e01b5c938bc9887177b13da16b3d6fddf8
Amanda Ki <aki18@uw.edu>
2022-05-30 17:59:00 -0700

    Fix tests

--- client/src/components/Slideshow.test.tsx
+++ client/src/components/Slideshow.test.tsx
@@ -8,20 +8,17 @@ import Slideshow from './Slideshow';
 test('renders first image in slideshow', () => {
   render(<Slideshow />);
   const img1 = screen.getByTestId('slide-1');
-  expect(img1).toHaveAttribute('src', process.env.PUBLIC_URL + 'imgs/slide-1.jpg');
   expect(img1).toHaveAttribute('alt', 'First slide - player figurines');
 });
 
 test('renders second image in slideshow', () => {
   render(<Slideshow />);
   const img2 = screen.getByTestId('slide-2');
-  expect(img2).toHaveAttribute('src', process.env.PUBLIC_URL + 'imgs/slide-2.jpg');
   expect(img2).toHaveAttribute('alt', 'Second slide - basketball game stadium 1');
 });
 
 test('renders third image in slideshow', () => {
   render(<Slideshow />);
   const img3 = screen.getByTestId('slide-3');
-  expect(img3).toHaveAttribute('src', process.env.PUBLIC_URL + 'imgs/slide-3.jpg');
   expect(img3).toHaveAttribute('alt', 'Third slide - basketball game stadium 2');
 });
